14a390be91ec0b381e77b0fe815c7d31e68c399e
Deian Stefan <deian@cs.ucsd.edu>
2019-11-07 00:33:38 -0800

    fix opt level + async benchmarks

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 build:
-	cd original && make build
-	cd rlbox && make build
-	npm i
+	-cd original && make build
+	-cd rlbox && make build
+	-npm i
 bench:
 	node bench.js
 	cloc --count-and-diff {original,rlbox}/src/bcrypt_node.cc
+clean:
+	-cd original && make clean
+	-cd rlbox && make clean
--- bench.js
+++ bench.js
@@ -7,7 +7,7 @@ Benchmark.options.delay = 5;
 Benchmark.options.initCount = 10;
 
 {
-  Benchmark.options.minSamples = 1000;
+  // Benchmark.options.minSamples = 1000;
   const suite = new Benchmark.Suite('genSaltSync');
   suite.add('rlbox-genSaltSync', () => {
     rlbox.genSaltSync();
@@ -19,7 +19,7 @@ Benchmark.options.initCount = 10;
     console.log(String(event.target));
   })
   .on('complete', function () {
-    console.log(`${this[1].name}/${this[0].name} = ${(this[1].hz/this[0].hz)}`);
+    console.log(`DONE: ${this[1].name}/${this[0].name} = ${(this[1].hz/this[0].hz)}`);
   })
   .run({ async: false });
 }
@@ -29,8 +29,7 @@ Benchmark.options.initCount = 10;
   const suite = new Benchmark.Suite('hashSync');
   // generate sensible random password
   const hash = crypto.randomFillSync(Buffer.alloc(32)).toString('hex');
-  // generate salt
-  const salt = rlbox.genSaltSync();
+  const salt = original.genSaltSync();
   suite.add('rlbox-hashSync', () => {
     rlbox.hashSync(hash, salt);
   })
@@ -41,7 +40,50 @@ Benchmark.options.initCount = 10;
     console.log(String(event.target));
   })
   .on('complete', function () {
-    console.log(`${this[1].name}/${this[0].name} = ${(this[1].hz/this[0].hz)}`);
+    console.log(`DONE: ${this[1].name}/${this[0].name} = ${(this[1].hz/this[0].hz)}`);
   })
   .run({ async: false });
+}
+
+{
+  Benchmark.options.minSamples = 1000;
+  const suite = new Benchmark.Suite('genSalt');
+  suite.add('rlbox-genSalt', {
+    defer: true,
+    fn: (d) => { rlbox.genSalt(() => { console.log('-');d.resolve(); }); }
+  })
+  .add('original-genSalt', {
+    defer: true,
+    fn: (d) => { original.genSalt(() => { console.log('+');d.resolve(); }); }
+  })
+  .on('cycle', (event) => {
+    console.log(String(event.target));
+  })
+  .on('complete', function () {
+    console.log(`DONE: ${this[1].name}/${this[0].name} = ${(this[1].hz/this[0].hz)}`);
+  })
+  .run({ async: false });
+}
+
+{
+  Benchmark.options.minSamples = 100; // Hashing is pretty slow
+  const suite = new Benchmark.Suite('hash');
+  // generate sensible random password
+  const hash = crypto.randomFillSync(Buffer.alloc(32)).toString('hex');
+  const salt = original.genSaltSync();
+  suite.add('rlbox-hash', { 
+    defer: true,
+    fn: (d) => { rlbox.hash(hash, salt, () => { d.resolve(); }); }
+  })
+  .add('original-hash', { 
+    defer: true,
+    fn: (d) => { original.hash(hash, salt, () => { d.resolve(); }); }
+  })
+  .on('cycle', (event) => {
+    console.log(String(event.target));
+  })
+  .on('complete', function () {
+    console.log(`DONE: ${this[1].name}/${this[0].name} = ${(this[1].hz/this[0].hz)}`);
+  })
+  .run({ async: true });
 }
